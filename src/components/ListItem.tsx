7dbe8efafbad9a29d25163c3ef786d66a6c87332
khinlei1998 <khinleiyatihtun98@gmail.com>
2023-11-03 16:26:06 +0630

    add listitem co

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,6 +1,6 @@
 import { View,Text } from "react-native"
 import { Fruit } from "../common/data"
-const Item: FC<Fruit> =(props)=>{
+const ListItem: FC<Fruit> =(props)=>{
     return(
         <View>
             <Text>{props.name}</Text>
