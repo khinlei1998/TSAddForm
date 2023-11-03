d1822e36ee99b02386b6dcbc483a659e0d44abd2
khinlei1998 <khinleiyatihtun98@gmail.com>
2023-11-03 16:06:45 +0630

    latest

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,9 +1,9 @@
 import { View,Text } from "react-native"
 import { Fruit } from "../common/data"
-// const Item: FC<Fruit> =(props)=>{
-//     return(
-//         <View>
-//             <Text>{props.name}</Text>
-//         </View>
-//     )
-// }
+const Item: FC<Fruit> =(props)=>{
+    return(
+        <View>
+            <Text>{props.name}</Text>
+        </View>
+    )
+}
